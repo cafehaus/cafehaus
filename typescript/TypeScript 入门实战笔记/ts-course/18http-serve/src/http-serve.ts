11e60248ecbb1ffb7772fa432c65fc91db1e0d2d
ZHOU <510878689@qq.com>
2022-06-12 14:52:27 +0800

    feat：新增http-serve

--- typescript/TypeScript 入门实战笔记/ts-course/18http-serve/src/http-serve.ts
+++ typescript/TypeScript 入门实战笔记/ts-course/18http-serve/src/http-serve.ts
@@ -1,8 +1,8 @@
 /// <reference path="../types.d.ts" />
 import http from 'http'
-// import ecstatic from 'ecstatic'
+import ecstatic from 'ecstatic'
 
-export interface |HttpServerOptions {
+export interface IHttpServerOptions {
     // 静态文件目录，默认是当前目录
     root?: string;
     // 缓存时间
@@ -10,18 +10,18 @@ export interface |HttpServerOptions {
 }
 
 // 对外暴露的方法
-export interface |HttpServer {
+export interface IHttpServer {
     // 启动服务
     listen(port: number): void;
     // 关闭服务
     close(): void
 }
 
-export default class HttpServer implements |HttpServer {
+export default class HttpServer implements IHttpServer {
     private server: http.Server
-    constructor(options: |HttpServerOptions) {
+    constructor(options: IHttpServerOptions) {
         const root = options.root || process.cwd()
-        this.server = http.createServer(ecstastic({
+        this.server = http.createServer(ecstatic({
             root,
             cache: options.cache === undefined ? 3600 : options.cache,
             showDir: true,
