b8ffb92675005f524a3f6bb2a99909b1628aa9fc
ZHOU <510878689@qq.com>
2022-12-04 01:21:02 +0800

    feat:spring boot: helloworld

---
+++ java/learn/SpringBoot/src/main/java/cn/cafe123/helloworld/controller/HelloController.java
@@ -0,0 +1,23 @@
+package cn.cafe123.helloworld.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world, cafehaus!";
+    }
+
+    // 带参数
+    @GetMapping("/username")
+    public String userName(String nickname) {
+        return "Hello," + nickname;
+    }
+
+    // post
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(String username, String password) {
+        return "你的名字：" + username + ", 你的密码：" + password;
+    }
+}
