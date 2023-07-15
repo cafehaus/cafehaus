6f3d223f9504c91cb3c547750afee4baef804a43
ZHOU <510878689@qq.com>
2023-07-16 00:19:45 +0800

    feat:新增员工

--- java/learn/sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ java/learn/sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -54,6 +60,50 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     * @return
+     */
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+
+        // employee.setIdNumber(employeeDTO.getIdNumber());
+        // employee.setName(employeeDTO.getName());
+        // employee.setPhone(employeeDTO.getPhone());
+        // employee.setSex(employeeDTO.getSex());
+        // employee.setUsername(employeeDTO.getUsername());
+
+        // 上面的一个一个设置属性也可以直接用下面的方法拷贝，注意要属性一致时才可以用
+        // 对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        // 设置用户状态
+        employee.setStatus(StatusConstant.ENABLE);
+
+        // 设置用户密码
+        String password = DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes());
+        employee.setPassword(password);
+
+        // 设置时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 设置创建/修改人
+        // TODO 后面需要完善
+        // employee.setCreateUser(10L);
+        // employee.setUpdateUser(10L);
+
+        // 完善方式，从 ThreadLocal 里取出从 token 里解析出来的员工 id
+        Long empId = BaseContext.getCurrentId();
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        // 插入数据库
+        employeeMapper.insert(employee);
     }
 
 }
