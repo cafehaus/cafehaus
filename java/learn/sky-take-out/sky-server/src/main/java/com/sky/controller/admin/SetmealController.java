81934a343b34525b818121cef83d52c4ec0303ab
ZHOU <510878689@qq.com>
2023-07-20 08:21:08 +0800

    feat:添加分页查询套餐列表

---
+++ java/learn/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("admin/setmeal")
+@Api(tags = "套餐")
+public class SetmealController {
+    @Autowired
+    SetmealService setmealService;
+
+    /**
+     * 分页查询套餐
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询套餐")
+    public  Result<PageResult> query(SetmealPageQueryDTO setmealPageQueryDTO) {
+        log.info("分页查询套餐:{}", setmealPageQueryDTO);
+        PageResult result = setmealService.query(setmealPageQueryDTO);
+        return Result.success(result);
+    }
+}
