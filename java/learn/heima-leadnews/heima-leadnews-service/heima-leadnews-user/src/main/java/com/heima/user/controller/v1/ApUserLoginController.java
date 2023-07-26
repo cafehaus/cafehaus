7e3ccb2b9593d13583a05c2f2f4570b0c5198927
ZHOU <510878689@qq.com>
2023-07-26 18:27:27 +0800

    feat:配置swagger和knife4j接口文档

--- java/learn/heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
+++ java/learn/heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/controller/v1/ApUserLoginController.java
@@ -4,20 +4,24 @@ package com.heima.user.controller.v1;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.user.dtos.LoginDto;
 import com.heima.user.service.ApUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/login")
+@Api(value = "登录", tags = "登录")
 @Slf4j
 public class ApUserLoginController {
     @Autowired
     private ApUserService apUserService;
 
     @PostMapping("/login_auth")
+    @ApiOperation("手机号密码登录")
     public ResponseResult login(@RequestBody LoginDto loginDto) {
-        log.info("登录：{}", loginDto);
+        log.info("手机号密码登录：{}", loginDto);
         return apUserService.login(loginDto);
     }
 }
