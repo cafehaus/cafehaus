101937a712c4c255ae9a3a1bcd5b2aecb40d1a35
ZHOU <510878689@qq.com>
2023-07-27 22:16:43 +0800

    feat:添加网管token校验过滤器

---
+++ java/learn/heima-leadnews/heima-leadnews-gateway/heima-leadnews-app-gateway/src/main/java/com/heima/app/gateway/filter/AuthorizeFilter.java
@@ -0,0 +1,61 @@
+package com.heima.app.gateway.filter;
+
+import com.heima.app.gateway.util.AppJwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthorizeFilter implements Ordered, GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+
+        // 登录接口不能拦截
+        if (request.getURI().getPath().contains("/login")) {
+            return chain.filter(exchange);
+        }
+
+        // 判断 token 是否存在
+        String token = request.getHeaders().getFirst("token");
+        if (StringUtils.isBlank(token)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        // 判断 token 是否过期
+        try {
+            Claims claimsBody = AppJwtUtil.getClaimsBody(token);
+            int result = AppJwtUtil.verifyToken(claimsBody);
+            if (result == 1 || result == 2) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+        } catch (Exception e) {
+            // throw new RuntimeException(e);
+            e.printStackTrace();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        // 放行
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
